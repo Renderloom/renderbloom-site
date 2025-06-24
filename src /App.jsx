53c0de9c36cb0593c9df598c2377d00699227348
Renderloom <adithya940a@gmail.com>
2025-06-24 19:07:36 +0000

    Test render

--- src /App.jsx
+++ src /App.jsx
@@ -1,22 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import React from 'react';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </>
-  );
+  return <h1>Hello from Renderbloom</h1>;
 }
 
 export default App;
